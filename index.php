e5e70e7bd0614041a82944e62ceedc6999e454f8
Azevedo <ubaporangacity@gmail.com>
2022-11-29 01:19:36 -0300

    first commit html, css, php

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hopeless Adventure</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="images/icon.png">
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+	<link href="../gameDownload/style/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<header>
+	<div class="center">
+			<div class="logo">
+				<a href=""><img src="images/logo.png" /></a>
+			</div>
+
+			<div class="menu">
+				<a href="">Sobre</a>
+				<a href="">Depoimentos</a>
+				<a class="btn-cta" href="">Baixar Agora!</a>
+			</div>
+			<div class="clear"></div>
+	</div>
+	</header>
+	<br />
+	<section class="topo">
+		<div class="center">
+			<div class="texto-topo">
+				<h2>Desafie seus amigos nesse multiplayer repleto de aventuras.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non accumsan urna. In at enim lectus. Nunc consectetur ut urna vel mattis.</p>
+				<br />
+				<a href=""><img src="images/appstore.png" /></a>
+				<a href=""><img src="images/googleplay.png" /></a>
+			</div>
+			<div class="img-topo">
+				<img src="images/hero.jpg" />
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+
+	<section class="depoimentos">
+		<div class="center">
+			<div class="depoimento-single">
+				<p>“O melhor jogo do ano! Incrível como consigo aprender e me divertir ao mesmo tempo!</p>
+				<br />
+				<span>Augusto Azevedo</span>
+			</div>
+			<div class="depoimento-single">
+				<p>“O melhor jogo do ano! Incrível como consigo aprender e me divertir ao mesmo tempo!</p>
+				<br />
+				<span>Augusto Azevedo</span>
+			</div>
+		</div>
+	</section>
+
+	<section class="sobre">
+		<div class="center">
+			<div class="titulo-sobre">
+				<h2>Sobre</h2>
+			</div>
+
+			<div class="sobre-single">
+
+				<div class="titulo-single">
+					<i class="icon-user"></i>
+					<h3>História</h3>
+				</div>
+
+				<div class="sobre-conteudo">
+					<p>Se aventure tentando superar todos seus desafios nesse jogo!</p>
+				</div>
+
+				<div class="clear"></div>
+			</div>
+			<div class="sobre-single">
+				<div class="titulo-single">
+					<i class="icon-user"></i>
+					<h3>Multiplayer</h3>
+				</div>
+				<div class="sobre-conteudo">
+					<p>Divirta-se com seus amigos e veja quem é o melhor!</p>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="sobre-single">
+				<div class="titulo-single">
+					<i class="icon-user"></i>
+					<h3>Conquistas</h3>
+				</div>
+				<div class="sobre-conteudo">
+					<p>Prove sua habilidade com as conquistas do jogo!</p>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+	<footer>
+		<div class="center">
+			<a href="https://github.com/azevedontc"><i class="icon-github"></i></a>
+		</div>
+	</footer>
+</body>
+</html>
